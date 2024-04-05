b8429032616c220760317b717e9783e85829d327
Archit Bansal <architbansal345@gmail.com>
2024-04-06 00:20:19 +0530

    dashboard_updated

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sort"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TeamData struct {
+	Teams []Team `json:"teams"`
+}
+
+type PlayerData struct {
+	Players []Player `json:"players"`
+}
+
+type Player struct {
+	Name               string `json:"Name"`
+	Wickets            int    `json:"Wickets"`
+	Runs               int    `json:"Runs"`
+	Catches            int    `json:"Catches"`
+	ManOfMatchesAwards int    `json:"ManOfMatchesAwards"`
+}
+
+type Team struct {
+	Name          string `json:"Name"`
+	MatchesPlayed int    `json:"MatchesPlayed"`
+	Wins          int    `json:"Wins"`
+	Losses        int    `json:"Losses"`
+	Points        int    `json:"Points"`
+}
+
+func sortByWickets(players []Player) {
+	sort.Slice(players, func(i, j int) bool {
+		return players[i].Wickets > players[j].Wickets
+	})
+}
+
+func sortByRuns(players []Player) {
+	sort.Slice(players, func(i, j int) bool {
+		return players[i].Runs > players[j].Runs
+	})
+}
+
+func sortByValue(players []Player) {
+	sort.Slice(players, func(i, j int) bool {
+		return calculateValue(players[i]) > calculateValue(players[j])
+	})
+}
+
+func calculateValue(player Player) int {
+	return player.Runs + 20*player.Wickets + 5*player.Catches + 100*player.ManOfMatchesAwards
+}
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("static/*.html")
+	r.GET("/dashboard", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	// Read team data from file
+	teamDataFile, err := ioutil.ReadFile("team.json")
+	if err != nil {
+		fmt.Println("Error reading team data:", err)
+		return
+	}
+	var teamData TeamData
+	err = json.Unmarshal(teamDataFile, &teamData)
+	if err != nil {
+		fmt.Println("Error unmarshalling team data:", err)
+		return
+	}
+
+	// Read player data from file
+	playerDataFile, err := ioutil.ReadFile("player.json")
+	if err != nil {
+		fmt.Println("Error reading player data:", err)
+		return
+	}
+	var playerData PlayerData
+	err = json.Unmarshal(playerDataFile, &playerData)
+	if err != nil {
+		fmt.Println("Error unmarshalling player data:", err)
+		return
+	}
+
+	// Routing
+	r.GET("/top-players-by-wickets", func(c *gin.Context) {
+		sortByWickets(playerData.Players)
+		c.HTML(http.StatusOK, "base.html", gin.H{
+			"Player": playerData.Players[:10], // Pass top 10 players
+		})
+	})
+
+	r.GET("/top-players-by-runs", func(c *gin.Context) {
+		sortByRuns(playerData.Players)
+		c.JSON(200, playerData.Players[:10])
+
+	})
+
+	r.GET("/top-players-by-value", func(c *gin.Context) {
+		sortByValue(playerData.Players)
+		c.JSON(200, playerData.Players[:10])
+	})
+
+	r.GET("/teams", func(c *gin.Context) {
+		sort.Slice(teamData.Teams, func(i, j int) bool {
+			return teamData.Teams[i].Points > teamData.Teams[j].Points
+		})
+		c.JSON(200, teamData.Teams)
+	})
+
+	// Run the server
+	r.Run(":8080")
+}
